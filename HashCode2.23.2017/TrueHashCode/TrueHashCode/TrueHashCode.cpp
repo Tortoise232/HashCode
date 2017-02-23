6cc3603b2fb33a728e7e78b1ad775cc94b85605b
Tortoise232 <petean.mihai232@yahoo.com>
2017-02-23 20:07:53 +0200

    data model

--- HashCode2.23.2017/TrueHashCode/TrueHashCode/TrueHashCode.cpp
+++ HashCode2.23.2017/TrueHashCode/TrueHashCode/TrueHashCode.cpp
@@ -2,13 +2,45 @@
 //
 
 #include "stdafx.h"
+#include <vector>
 
+class Video {
+public:
+	int size;
+	int id;
+};
 
+class Requests {
+public:
+	int videoID;
+	int endpointID;
+	int value; //cate requesturi sunt facute de la acel endpoint
+};
 
+class  Endpoint {
+public:
+	int id;
+	std::vector<Video> videos;
+	int dataCenterLatency;
+	// pair pentru ca fata de fiecare 
+	//cache este un anumit latency   std::pair<cache,latency>
+	std::vector<std::pair<int, Cache&>> cacheLatencies; 
+	Endpoint(std::vector<int> videoRequests, int dataCenterLatency) {
 
+	}
+};
+
+//videos are just ints;
+//caches;
+
+class Cache {
+public:
+	static int maxSize;
+};
 
 int main()
 {
+
 	//Mihai e paros!
     return 0;
 }
