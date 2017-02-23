66fd5c1aeaf21601e8fa22d814d1512157220b49
Tortoise232 <petean.mihai232@yahoo.com>
2017-02-23 21:16:09 +0200

    fileReadingWorks

--- HashCode2.23.2017/TrueHashCode/TrueHashCode/TrueHashCode.cpp
+++ HashCode2.23.2017/TrueHashCode/TrueHashCode/TrueHashCode.cpp
@@ -3,18 +3,32 @@
 
 #include "stdafx.h"
 #include <vector>
+#include <map>
+#include <fstream>
+#include <iostream>
+
 
 class Video {
 public:
 	int size;
 	int id;
+	Video(int size, int id) {
+		this->size = size;
+		this->id = id;
+	}
 };
 
-class Requests {
+class Request {
 public:
 	int videoID;
 	int endpointID;
 	int value; //cate requesturi sunt facute de la acel endpoint
+	Request(int videoID, int endpointID, int value) {
+		this->videoID = videoID;
+		this->endpointID = endpointID;
+		this->value = value;
+
+	}
 };
 
 class  Endpoint {
@@ -25,8 +39,11 @@ public:
 	// pair pentru ca fata de fiecare 
 	//cache este un anumit latency   std::pair<cacheID,latency>
 	std::vector<std::pair<int, int>> cacheLatencies; 
-	Endpoint(std::vector<int> videoRequests, int dataCenterLatency) {
-
+	
+	Endpoint(int dataCenterLatency, std::vector<std::pair<int, int>> cacheLatencies, int id) {
+		this->dataCenterLatency = dataCenterLatency;
+		this->cacheLatencies = cacheLatencies;
+		this->id = id;
 	}
 };
 
@@ -35,24 +52,72 @@ public:
 	static int maxSize;
 	int currentSize;
 	std::vector<Video> actualVideos;
+
 };
 
 //videos are just ints;
 //caches;
 
+
+
 std::vector<Video> videos;
 std::vector<Cache> caches;
-std::vector<Requests> requests;
+std::vector<Request> requests;
+std::vector <Endpoint> endpoints;
+
+
 
 void readData(char* filename) {
-	std::ifstream
+	std::ifstream data(filename, std::ios::binary);
+	int V, E, R, C, Csize;
+	int videoSize;
+	//reads nr of videos, nr of endpoints, nr of requests, nr of caches, max cache size in this order
+	data >> V >> E >> R >> C >> Csize;
+	
+
+	//read videos
+	for (int ct = 0; ct < V; ct++) {
+		data >> videoSize;
+		videos.push_back(*(new Video(videoSize, ct)));// reads videos;
+	}
+
+	//read endpoints
+	int latencyToServer, nrOfConnections, cacheNr, cacheLatency;
+	std::vector<std::pair<int, int>> tempPairs;
+	
+	
+	
+	for (int ct = 0; ct < E; ct++) {
+		data >> latencyToServer >> nrOfConnections;
+		tempPairs.clear();
+		for (int ct2 = 0; ct2 < nrOfConnections; ct2++) {
+			data >> cacheNr >> cacheLatency;
+			tempPairs.push_back(*(new std::pair<int, int>(cacheNr, cacheLatency)));
+		}
+		endpoints.push_back(*(new Endpoint(latencyToServer, tempPairs, ct)));
+	}
+
+	//read requests
+	int videoID, endpointID, value;
+	for (int ct = 0; ct < R; ct++) {
+		data >> videoID >> endpointID >> value;
+		requests.push_back(*(new Request(videoID, endpointID, value)));
+
+	}
+	
+
 }
 
 int main()
 {
 
-	
+	readData("me_at_the_zoo.in");
+	std::cout << videos.size() << "\n";
+	std::cout << endpoints.size() << "\n";
+	std::cout << requests.size() << "\n";
+	std::cout << caches.size() << "\n";
 	//Mihai e paros!
+
     return 0;
 }
 
